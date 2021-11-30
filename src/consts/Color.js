04e2f81cddff1cef608469663a6639b42ffba97c
Ratty <77925701+rattybox@users.noreply.github.com>
2021-11-30 12:04:11 +0700

    Create Color.js

---
+++ src/consts/Color.js
@@ -0,0 +1,25 @@
+const BoxOrange = 6
+const BoxRed = 7
+const BoxBlue = 8
+const BoxGreen = 9
+const BoxGray = 10
+
+const TargetOrange = 25
+const TargetRed = 38
+const TargetBlue = 51
+const TargetGreen = 64
+const TargetGray = 77
+
+export {
+    BoxOrange,
+    BoxRed, 
+    BoxBlue,
+    BoxGreen,
+    BoxGray,
+
+    TargetOrange,
+    TargetRed,
+    TargetBlue,
+    TargetGreen,
+    TargetGray
+}
