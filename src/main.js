582ea32f118c3daf096026d3cbc67aaa2fd09558
Your Name <you@example.com>
2021-12-04 22:09:01 +0700

    CHANGE Map Data mechanics

--- src/main.js
+++ src/main.js
@@ -1,32 +1,35 @@
 import LevelCore from "./scenes/LevelCore.js";
 import {
-    LevelMap
+  LevelMap
 } from "./consts/LevelMap.js";
 import Start from "./scenes/Start.js";
 import Narration from "./scenes/Narration.js";
 import Help from "./scenes/Help.js";
+import {
+  convertToNumber
+} from "./consts/LevelMap.js";
 
 const levelKeysArr = Object.keys(LevelMap);
 const levelArr = [Start, Narration, Help];
 
 for (let i = 0; i < levelKeysArr.length; i++) {
-    levelArr.push(new LevelCore(levelKeysArr[i], LevelMap[levelKeysArr[i]], levelKeysArr[(i + 1) % levelKeysArr.length]));
+  levelArr.push(new LevelCore(levelKeysArr[i], convertToNumber(LevelMap[levelKeysArr[i]]), levelKeysArr[(i + 1) % levelKeysArr.length]));
 }
 
 
 const config = {
-    type: Phaser.AUTO,
-    width: 1152,
-    height: 1152,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: {
-                y: 200
-            }
-        }
-    },
-    scene: levelArr
+  type: Phaser.AUTO,
+  width: 1152,
+  height: 1152,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {
+        y: 200
+      }
+    }
+  },
+  scene: levelArr
 };
 
 let game = new Phaser.Game(config);
