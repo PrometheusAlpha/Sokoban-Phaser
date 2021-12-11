00bf9117dadc40330918552e20e43350b32ce4d5
Ratty <77925701+rattybox@users.noreply.github.com>
2021-12-11 09:53:06 +0700

    Update NameInput.js

--- src/scenes/NameInput.js
+++ src/scenes/NameInput.js
@@ -7,7 +7,7 @@ export default class NameInput extends Phaser.Scene {
   preload() {}
 
   create() {
-    let player = prompt("Please enter your name", "Player") || "Player";
+    let player = prompt("Please enter your name (default will be Player)", "Player") || "Player";
     this.registry.set('name', player);
     this.scene.start('Narration');
   }
